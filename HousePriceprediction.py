e8dcdaeec5a400f2ea9388f5a22806ecac07a5ea
Apoorva Sharan <83828913+apoorva458@users.noreply.github.com>
2023-06-27 00:26:31 +0530

    Add files via upload

---
+++ HousePriceprediction.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the dataset
+data = pd.read_csv('house_data.csv')
+
+# Split the data into features (X) and target variable (y)
+X = data.drop('price', axis=1)
+y = data['price']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print('Mean Squared Error:', mse)
+print('R-squared:', r2)
